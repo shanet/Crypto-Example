8c9151d09bacfbdf4fe99ebe9e1d4ec0600f2134
shane tully <shane@shanetully.com>
2013-08-24 18:10:15 -0700

    added more comments

--- crypto-example.cpp
+++ crypto-example.cpp
@@ -13,6 +13,7 @@ int main() {
     Crypto crypto;
 
     #ifdef PRINT_KEYS
+        // Write the keys to stdout if compiled as such
         crypto.writeKeyToFile(stdout, KEY_SERVER_PRI);
         crypto.writeKeyToFile(stdout, KEY_SERVER_PUB);
         crypto.writeKeyToFile(stdout, KEY_CLIENT_PUB);
@@ -38,26 +39,29 @@ int main() {
     size_t ivl;
 
     while(1) {
+        // Get the message to encrypt
         printf("Message to RSA encrypt: ");
         fflush(stdout);
         getline(cin, msg);
 
-        if(!msg.compare("exit")) break;
-
+        // Encrypt the message with RSA
         if((encMsgLen = crypto.rsaEncrypt((const unsigned char*)msg.c_str(), msg.size(), &encMsg, &ek, &ekl, &iv, &ivl)) == -1) {
             fprintf(stderr, "Encryption failed\n");
             return 1;
         }
 
+        // Print the encrypted message as a base64 string
         char* b64String = base64Encode(encMsg, encMsgLen);
         printf("Encrypted message: %s\n", b64String);
 
+        // Decrypt the message
         if((decMsgLen = crypto.rsaDecrypt(encMsg, (size_t)encMsgLen, ek, ekl, iv, ivl, (unsigned char**)&decMsg)) == -1) {
             fprintf(stderr, "Decryption failed\n");
             return 1;
         }
         printf("Decrypted message: %s\n", decMsg);
 
+        // No one likes memory leaks
         free(encMsg);
         free(decMsg);
         free(ek);
@@ -69,19 +73,22 @@ int main() {
         iv        = NULL;
         b64String = NULL;
 
+        // Get the message to encrypt
         printf("Message to AES encrypt: ");
         fflush(stdout);
         getline(cin, msg);
 
-         if((encMsgLen = crypto.aesEncrypt((const unsigned char*)msg.c_str(), msg.size(), &encMsg)) == -1) {
+        // Encrypt the message with AES
+        if((encMsgLen = crypto.aesEncrypt((const unsigned char*)msg.c_str(), msg.size(), &encMsg)) == -1) {
             fprintf(stderr, "Encryption failed\n");
             return 1;
         }
 
+        // Print the encrypted message as a base64 string
         b64String = base64Encode(encMsg, encMsgLen);
         printf("Encrypted message: %s\n", b64String);
 
-
+        // Decrypt the message
         if((decMsgLen = crypto.aesDecrypt(encMsg, (size_t)encMsgLen, (unsigned char**)&decMsg)) == -1) {
             fprintf(stderr, "Decryption failed\n");
             return 1;
@@ -89,6 +96,7 @@ int main() {
         printf("%d bytes decrypted\n", decMsgLen);
         printf("Decrypted message: %s\n", decMsg);
 
+        // Memory leaks... yadda yadda yadda...
         free(encMsg);
         free(decMsg);
         free(b64String);
